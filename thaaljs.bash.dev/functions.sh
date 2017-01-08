4e1ecbb5de70593e0c0a7a51978f40290f0bd604
Pranav Rana <prnvrna@gmail.com>
2017-01-08 12:17:40 +0530

    improved bash examples

--- thaaljs.bash.dev/functions.sh
+++ thaaljs.bash.dev/functions.sh
@@ -15,3 +15,35 @@ function print_general_info {
 	printf "PID of the last command excuted in the background is: %d\n" $!
 	printf "Options flag that is currently in effect is: %s\n" $-
 }
+
+function print_arguments_on_seaparate_lines {
+	printf "%s\n" Print arguments on "separate lines"
+}
+
+function using_example_percent_b {
+	printf "%b\n" "Hello\nWorld" "12\tword"
+}
+
+function print_as_decimal {
+	printf "%.2f\n" $1
+}
+
+function print_as_exponential {
+	printf "%e\n" $1
+}
+
+function print_as_hex {
+	printf "%x\n" $1
+}
+
+function print_color_code_from_rgb_to_hex {
+	printf "#%02x%02x%02x\n" $1 $2 $3
+}
+
+function print_flush_example_one {
+	printf "%8s %-15s:\n" first second third fourth fifth sixth
+}
+
+function print_flush_example_two {
+	printf "%04d\n" 12 23 56 123 255 2121
+}
--- thaaljs.bash.dev/main.sh
+++ thaaljs.bash.dev/main.sh
@@ -3,5 +3,17 @@
 # including our functions
 . "$(basename $(dirname $0))/functions.sh"
 # handling execution
-print_header $1
-print_general_info
+print_header $1 # passing delimeter to the function
+# examples
+##print_as_decimal 0xff
+##print_as_decimal 011
+##print_as_exponential 0xff
+##print_as_exponential 011
+##print_as_hex 1120900
+##print_as_hex 011
+##print_as_hex 10
+##print_color_code_from_rgb_to_hex 65 105 225
+##print_color_code_from_rgb_to_hex 255 255 255
+##print_color_code_from_rgb_to_hex 0 0 0
+##print_flush_example_one
+##print_flush_example_two
