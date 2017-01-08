1d7d1c74e078d53da30b0038dbd009f6fec8778a
Pranav Rana <prnvrna@gmail.com>
2017-01-08 09:57:47 +0530

    Introduced Bash examples

--- thaaljs.bash.dev/main.sh
+++ thaaljs.bash.dev/main.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+#: author: "pranav rana" <pranav@dwellupper.io>
+#: version: 1.0
+#: date started: Jan 8 2017
 
-echo "ello planet earth !"
+printf "%d\n" 200
+echo "Content-Type: text/html; charset=utf-8"
+printf "%s" $1
+printf "%s\n" "ello planet earth!"
